e6f9edf121bdf350abf8ebdfb5a99fd2e68a48fb
Trent Endresen <trentendresen@Trents-MacBook-Pro.local>
2022-10-19 16:17:25 -0500

    Revert again

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "balderweb": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
